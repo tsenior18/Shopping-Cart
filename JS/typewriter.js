0bd107f2bb1a6ea02dced5fe3a16f9fe477dac81
Nanhe Kumar <tomsenior18@gmail.com>
2020-05-25 18:39:01 +0100

    Price section added with descriptions

--- JS/typewriter.js
+++ JS/typewriter.js
@@ -1,9 +1,12 @@
+// Setting Default Index position to 0
 var i = 0,
     text
 text = 'Welcome To the Shopping Page'
 
+
 function typingEffect() {
     if (i < text.length) {
+        // Adding the relevant Index position to the page
         document.getElementById("h1").innerHTML += text.charAt(i);
         i++
         setTimeout(typingEffect, 50);
